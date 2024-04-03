fa36b3ea122ecd72a405f8f9b91660dd274b94cc
HP <HP@DESKTOP-NJ2DTAU>
2024-04-03 13:41:39 +0530

    HelloWorld

--- src/com/tnsif/sample/Day01.java
+++ src/com/tnsif/sample/Day01.java
@@ -1,5 +1,10 @@
 package com.tnsif.sample;
 
 public class Day01 {
+	public static void main (String[]args)
+	{
+	System.out.println("C2TC program for jp");
+	System.out.print("c2tc program");
 
+}
 }
